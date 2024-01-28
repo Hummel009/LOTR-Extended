6a303ce7443d491d3eaa6a5a76593339506f8b6e
Hummel009 <hummel.turbamentum@gmail.com>
2024-01-28 19:16:14 +0300

    Update LOTRExtended.java

--- src/main/java/lotre/LOTRExtended.java
+++ src/main/java/lotre/LOTRExtended.java
@@ -12,7 +12,7 @@ import lotre.proxy.LEServerProxy;
 import lotre.util.LEAPI;
 import net.minecraft.util.ResourceLocation;
 
-@Mod(modid = "lotre", dependencies = "required-after:lotr")
+@Mod(modid = "lotre", dependencies = "required-after:lotr", useMetadata = true)
 public class LOTRExtended {
 	@SidedProxy(clientSide = "lotre.proxy.LEClientProxy", serverSide = "lotre.proxy.LEServerProxy")
 	private static LEServerProxy proxy;
